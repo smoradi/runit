c503ba3b230b36d1aca6d2b84413fd9b7a980b3c
fredo <fredo@ubuntu-10.04.3>
2012-05-19 13:46:41 +0100

    ruby calctest.rb to run tests.

---
+++ calcrun.rb
@@ -0,0 +1,20 @@
+
+class CalcRun
+
+  def plus(n1, n2)
+    n1 + n2
+  end
+
+  def mines(n1, n2)
+    n1 - n2
+  end
+
+  def multiple(n1, n2)
+    n1 * n2
+  end
+
+  def divide(n1, n2)
+   n1 / n2
+  end
+
+end
---
+++ calctest.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+require File.expand_path(File.dirname(__FILE__) + '/testcase')
+require File.expand_path(File.dirname(__FILE__) + '/testsuite')
+require File.expand_path(File.dirname(__FILE__) + '/testresult')
+require File.expand_path(File.dirname(__FILE__) + '/calcrun')
+
+#require 'test/unit/assertions'
+
+class CalcTestCase < TestCase
+  def setUp
+    @calc= CalcRun.new
+  end
+  def testPlus
+    assert_equal 2, @calc.plus(1,1)
+    assert_equal 0, @calc.plus(-1,1)
+    assert_equal -1, @calc.plus(0, -1)
+    assert_equal "one", @calc.plus("", "one")
+  end
+  def testMines
+    assert_equal 1, @calc.mines(2,1)
+#    assert_equal "on", @calc.mines("one", "e")
+  end
+  def testMultiple
+    assert_equal 4, @calc.multiple(2,2)
+  end
+  def testDivide
+    assert_equal 2, @calc.divide(4,2)
+  end
+  def testMixOfActions
+    assert_equal 1, (@calc.mines(@calc.plus(2,2), 3))
+    assert_equal 1, (@calc.divide(@calc.multiple(2,2), 4))
+  end
+
+end
+
+if __FILE__ == $0
+  suite= TestSuite.new
+  suite.add(CalcTestCase.new("testPlus"))
+  suite.add(CalcTestCase.new("testMines"))
+  suite.add(CalcTestCase.new("testMultiple"))
+  suite.add(CalcTestCase.new("testDivide"))
+  suite.add(CalcTestCase.new("testMixOfActions"))
+  result= TestResult.new
+  suite.run(result)
+  puts result.summary()
+end
